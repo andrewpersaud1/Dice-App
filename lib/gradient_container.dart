088d60fbba3db1198b0219629bd7f732b5cecd5a
andrewpersaud1 <andrewpersaud022@gmail.com>
2023-05-15 15:30:22 -0400

    Second push

---
+++ lib/gradient_container.dart
@@ -0,0 +1,26 @@
+import 'package:dice_app/styled_text.dart';
+import 'package:flutter/material.dart';
+
+import 'package:dice_app/gradient_container.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+        colors: [
+          Color.fromARGB(255, 26, 2, 80),
+          Color.fromARGB(255, 45, 7, 98),
+        ],
+        begin: startAlignment,
+        end: endAlignment,
+      )),
+      child: const Center(child: StyledText('Hello')),
+    );
+  }
+}
