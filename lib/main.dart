e6c4a907493dfb260deac7a6b04b9d5b51883b32
andrewpersaud1 <andrewpersaud022@gmail.com>
2023-05-22 17:07:53 -0400

    finishing up

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,10 @@ import 'gradient_container.dart';
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
-      body: const GradientContainer(),
+      body: GradientContainer(
+        Color.fromARGB(255, 1, 1, 0),
+        Color.fromARGB(255, 141, 1, 1),
+      ),
     ),
   ));
 }
